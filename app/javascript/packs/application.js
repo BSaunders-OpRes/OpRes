43a0ea3cfca4b926dd3235185b6d415cfb22bcae
Azan Abrar <azan.abrar@gmail.com>
2021-04-16 17:14:48 +0500

    UI of the application admin panel.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
+import "../stylesheets/stylesheet";
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
