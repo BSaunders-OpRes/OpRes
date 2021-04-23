6484c0970e487580dadb061c8260cdcefef709a1
Azan Abrar <azan.abrar@gmail.com>
2021-04-23 12:58:14 +0500

    UI updates in the login, BSL and supplier screens.

---
+++ app/javascript/javascripts/javascript.js
@@ -0,0 +1,1 @@
+import './bsl/bsl_js';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("bootstrap");
 import JQuery from 'jquery';
 window.$ = window.JQuery = JQuery;
 
+import "../javascripts/javascript.js";
 import "../stylesheets/stylesheet.scss";
-
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
