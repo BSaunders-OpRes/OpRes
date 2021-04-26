0cd9090f0e96d296aab18bd18d5b7669bb0e0317
Azan Abrar <azan.abrar@gmail.com>
2021-04-26 12:42:21 +0500

    WIP coustomer journey.

--- app/controllers/organisation/journeys_controller.rb
+++ app/controllers/organisation/journeys_controller.rb
@@ -21,6 +21,20 @@ class Organisation::JourneysController < Organisation::BaseController
   end
 
   def institution_step
+    countries_units = []
+    regions         = Unit::regions
+    regions&.each do |region|
+      countries = params[region] && params.dig(region).values.first
+
+      countries&.each do |country|
+        countries_units << Units::Country.new(
+          parent_id: params.dig(region).keys.first.to_i,
+          region:    region,
+          name:      Units::Country.build_name(organisational_unit, country)
+        )
+      end
+      Units::Country.import(countries_units, on_duplicate_key_ignore: true)
+    end
   end
 
   def product_step
--- app/models/units/country.rb
+++ app/models/units/country.rb
@@ -1,2 +1,7 @@
 class Units::Country < Unit
+  class << self
+    def build_name(organization,country)
+      "#{organization.name} #{country}"
+    end
+  end
 end
