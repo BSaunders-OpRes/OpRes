efdc7fa5924cc6755e22353bcc1167487c5851c0
Azan Abrar <azan.abrar@gmail.com>
2021-04-26 16:49:51 +0500

    Added organisation sidebar and header.

--- app/javascript/javascripts/bsl/bsl_js.js
+++ app/javascript/javascripts/bsl/bsl_js.js
@@ -1,11 +1,31 @@
-$(document).ready(function(){
+document.addEventListener("turbolinks:load", () => {
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
-})
+  });
 
+  $('#close_sidebar').click(function() {
+    $('.sidebar').css('width', '0');
+    $('.content').removeClass('blur');
+    $('.header').removeClass('blur');
+  });
 
-document.addEventListener("turbolinks:load", () => { 
+  $('#open_sidebar').click(function() {
+    $('.sidebar').css('width', '290');
+    $('.content').addClass('blur');
+    $('.header').addClass('blur');
+  });
+
+  $('#bsl-tab li').click(function() {
+    href  = $(this).find('a').attr('href');
+    parts = href.split('-');
+    parts.splice(-1);
+    parts.push('desc');
+    desc = parts.join('-');
+
+    $('#bsl-desc p').addClass('d-none');
+    $(desc).removeClass('d-none');
+  });
+
   document.querySelector("#myinput_one").oninput = function() {
     var value = (this.value-this.min)/(this.max-this.min)*100
     this.style.background = 'linear-gradient(to right, #000 0%, #000 ' + value + '%, #F3F5FA ' + value + '%, #F3F5FA 100%)'
--- app/javascript/javascripts/javascript.js
+++ app/javascript/javascripts/javascript.js
@@ -1,1 +1,3 @@
+require.context('../images', true);
+
 import './bsl/bsl_js';
