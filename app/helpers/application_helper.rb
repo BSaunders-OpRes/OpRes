daf6e6bb4d3ac50e64543c6a1886de866639411c
Azan Abrar <azan.abrar@gmail.com>
2021-04-27 16:44:03 +0500

    Dynamic unit admin invitation flow.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def new_or_create
     params[:action].in? %w[new create]
   end
+
+  def organisation_dropdown_options
+    childs = organisational_unit.inclusive_children
+    childs = Unit.sort_children(childs)
+    childs.pluck(:name, :id)
+  end
 end
--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,4 +6,13 @@ class InvitationMailer < ApplicationMailer
 
     mail(to: @user.email, subject: "Join #{@organisational_unit.name}!")
   end
+
+  def invite_unit_admin(user, password)
+    @user     = user
+    @password = password
+    @organisational_unit = @user.unit
+    @managing_unit       = @user.managing_unit
+
+    mail(to: @user.email, subject: "Join #{@managing_unit.name}!")
+  end
 end
