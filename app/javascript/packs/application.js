f3756ae670accb94eb947b3ff3ffb440406393ff
Azan Abrar <azan.abrar@gmail.com>
2021-04-30 16:48:28 +0500

    Designed administration portal and management dashboard.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,20 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
 
-require("jquery");
-require("popper.js");
-require("bootstrap");
+require('jquery');
+require('popper.js');
+require('bootstrap');
 
-import JQuery from 'jquery';
+import JQuery     from 'jquery';
+import Highcharts from 'highcharts';
+
 window.$ = window.JQuery = JQuery;
+window.Highcharts = Highcharts;
 
-import "../javascripts/javascript.js";
-import "../stylesheets/stylesheet.scss";
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
+import '../javascripts/javascript.js';
+import '../stylesheets/stylesheet.scss';
+var componentRequireContext = require.context('components', true);
+var ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(componentRequireContext);
