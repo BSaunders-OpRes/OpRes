eebcbdfab312088d298582da0a4d616e995a7cec
Azan Abrar <azan.abrar@gmail.com>
2021-07-16 15:36:28 +0500

    Dynamic supplier show page and updated json file of institutions

---
+++ app/services/graphs/supplier_critical_important_bsl_service.rb
@@ -0,0 +1,45 @@
+class Graphs::SupplierCriticalImportantBslService < ApplicationService
+  COLORS = %w[#6BEAB3 #367C5C #CDFAF1 #05b368]
+
+  def initialize(args)
+    @supplier = Supplier.find(args[:supplier])
+    @data     = {}
+  end
+
+  attr_reader :supplier, :data
+
+  def call
+    data[:overall] = overall
+
+    data
+  end
+
+  private
+
+  def overall
+    datum = {}
+
+    critical_supplier_step  = supplier.critical_supplier_steps
+    important_supplier_step = supplier.important_supplier_steps
+
+    datum[:supplier] = supplier
+    datum[:total]    = critical_supplier_step.size + important_supplier_step.size
+    datum[:graph]    = []
+
+    datum[:graph] << {
+      name:  'Critical',
+      y:     datum[:total].zero? ? 0 : ((critical_supplier_step.size / datum[:total].to_f) * 100).round(2),
+      color: COLORS[0],
+      count: critical_supplier_step.size
+    }
+
+    datum[:graph] << {
+      name:  'Important',
+      y:     datum[:total].zero? ? 0 : ((important_supplier_step.size / datum[:total].to_f) * 100).round(2),
+      color: COLORS[1],
+      count: important_supplier_step.count
+    }
+
+    datum
+  end
+end
