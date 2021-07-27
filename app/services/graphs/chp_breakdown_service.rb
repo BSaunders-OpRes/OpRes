f47db106cfbfe4991b985ffa82038332b9ebb4a4
Shahzad Ali <shahzad.ali@eritheialabs.com>
2021-07-27 17:19:08 +0500

    cloud service provider breakdown chart

--- app/services/graphs/chp_breakdown_service.rb
+++ app/services/graphs/chp_breakdown_service.rb
@@ -3,86 +3,30 @@ class Graphs::ChpBreakdownService < ApplicationService
 
   def initialize(args)
     @data = {}
+    @args = args
     @organisational_unit = args['organisational_unit']
     @current_user = args['current_user']
   end
-  attr_reader :data
-  
+
+  attr_reader :args, :data
+
   def call
-    data[:overall]   = overall
-    #data[:breakdown] = breakdown
+    nodes     = args.dig('organisational_unit').inclusive_children.map(&:id)
+    suppliers = Supplier.where(unit_id: nodes).group_by(&:cloud_hosting_provider)
 
-    data  
-  end
+    data[:total] = suppliers.values.flatten.size
+    data[:graph] = []
 
-  private
-
-  def overall
-    datum = {}
-    chp_suppliers = Supplier.where(unit_id: @organisational_unit.inclusive_children.pluck(:id))
-
-    datum[:total] = chp_suppliers.size
-    datum[:graph] = []
-
-    CloudHostingProvider.all.each_with_index do |chp, index|
-      suppliers = chp_suppliers[chp] || []
-      datum[:graph] << {
-        name:  chp.short_name,
-        y:     datum[:total].zero? ? 0 : ((suppliers.size / datum[:total].to_f) * 100).round(2),
-        color: COLORS[index],
-        value: suppliers.size,
-        logo:  chp.logo
+    CloudHostingProvider.all.each_with_index do |obj, number|
+      spls = suppliers[obj] || []
+      data[:graph] << {
+        name: obj.short_name,
+        y: data[:total].zero? ? 0 : ((spls.size / data[:total].to_f) * 100).round(2),
+        color: COLORS[number],
+        value: spls.size
       }
     end
 
-    datum
-  end
-
-  def breakdown
-    datum = {}
-
-    critical_suppliers  = Supplier.joins(supplier_steps: [step: [:business_service_line]])
-                                  .where(business_service_lines: { id: bsl.id })
-                                  .where(supplier_steps: { importance_level: SupplierStep.importance_levels[:critical] })
-                                  .group_by { |s| s.cloud_hosting_provider.name }
-                                  .each_with_object({}) {|(k, v), h| h[k] = v.group_by { |s| s.consumption_model } }
-
-    important_suppliers = Supplier.joins(supplier_steps: [step: [:business_service_line]])
-                                  .where(business_service_lines: { id: bsl.id })
-                                  .where(supplier_steps: { importance_level: SupplierStep.importance_levels[:important] })
-                                  .group_by { |s| s.cloud_hosting_provider.name }
-                                  .each_with_object({}) {|(k, v), h| h[k] = v.group_by { |s| s.consumption_model } }
-
-    CloudHostingProvider.all.each do |chp|
-      datum[chp.name] = {
-        name: chp.name,
-        logo: chp.logo,
-        consumption_models: {}
-      }
-      Supplier.real_consumption_models.keys.each do |cm|
-        datum[chp.name][:consumption_models][cm] = {}
-
-        critical_count   = (critical_suppliers.dig(chp.name, cm)&.size  || 0)
-        importance_count = (important_suppliers.dig(chp.name, cm)&.size || 0)
-        total_count      = critical_count + importance_count
-
-        datum[chp.name][:consumption_models][cm][:total] = total_count
-        datum[chp.name][:consumption_models][cm][:graph] = []
-        datum[chp.name][:consumption_models][cm][:graph] << {
-          name:     'critical',
-          y:        total_count.zero? ? 0 : ((critical_count / total_count.to_f) * 100).round(2),
-          color:    COLORS[0],
-          value: critical_count
-        }
-        datum[chp.name][:consumption_models][cm][:graph] << {
-          name:     'important',
-          y:        total_count.zero? ? 0 : ((importance_count / total_count.to_f) * 100).round(2),
-          color:    COLORS[1],
-          value: importance_count
-        }
-      end
-    end
-
-    datum
+    data
   end
 end
