5506df671ac4989649699b6e705a6b67e875f82a
Azan Abrar <azan.abrar@gmail.com>
2021-07-29 11:33:31 +0500

    inherit the supplier_conformance_service with base service and update service file

--- app/services/graphs/supplier_conformance_impact_tolerance_service.rb
+++ app/services/graphs/supplier_conformance_impact_tolerance_service.rb
@@ -1,18 +1,13 @@
-class Graphs::SupplierConformanceImpactToleranceService < ApplicationService
+class Graphs::SupplierConformanceImpactToleranceService < Graphs::BaseService
   COLORS   = %w[#6BEAB3 #367C5C #CDFAF1 #05b368]
   SLA_ATTR = %w[service_level_agreement service_level_objective recovery_point_objective recovery_point_objective
                 severity1 severity2 severity3 severity4 severity1_restoration severity2_restoration severity3_restoration 
                 severity4_restoration recovery_time_objective
                ]
-
-  def initialize(args)
-    @supplier = Supplier.find(args[:supplier])
-    @data     = {}
-  end
-
-  attr_reader :supplier, :data
+  attr_reader :supplier
 
   def call
+    @supplier = Supplier.find(args.dig('supplier'))
     data[:overall]   = overall
 
     data
@@ -22,9 +17,8 @@ class Graphs::SupplierConformanceImpactToleranceService < ApplicationService
 
   def overall
     datum = {}
-
     bsls = BusinessServiceLine.joins(steps: [supplier_steps: [:supplier]])
-                              .where(suppliers: {id: @supplier.id})
+                              .where(suppliers: {id: supplier.id})
                               .includes(:sla)
 
     total = bsls.count * 120
