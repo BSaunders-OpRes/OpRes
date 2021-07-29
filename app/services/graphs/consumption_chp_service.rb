6ca4355806f4701b15f4e6bcd2e7c5ea79a2b47e
Shahzad Ali <shahzad.ali@eritheialabs.com>
2021-07-29 19:12:54 +0500

    overview CHP dashboard done

--- app/services/graphs/consumption_chp_service.rb
+++ app/services/graphs/consumption_chp_service.rb
@@ -1,23 +1,22 @@
-class Graphs::ChpBreakdownService < Graphs::BaseService
+class Graphs::ConsumptionChpService < Graphs::BaseService
   # CHP breakdown of suppliers.
 
   COLORS = %w[#6BEAB3 #367C5C #CDFAF1 #05b368]
 
   def call
     nodes     = organisational_unit.inclusive_children.map(&:id)
-    suppliers = Supplier.where(unit_id: nodes).group_by(&:cloud_hosting_provider)
+    suppliers = Supplier.where(unit_id: nodes).group_by(&:consumption_model)
 
     data[:total] = suppliers.values.flatten.size
     data[:graph] = []
 
-    CloudHostingProvider.all.each_with_index do |obj, number|
+    Supplier.consumption_models.each do |obj, number|
       spls = suppliers[obj] || []
       data[:graph] << {
-        name:  obj.short_name,
+        name:  obj.humanize,
         y:     data[:total].zero? ? 0 : ((spls.size / data[:total].to_f) * 100).round(2),
         color: COLORS[number],
-        value: spls.size,
-        logo: obj.logo
+        value: spls.size
       }
     end
 
