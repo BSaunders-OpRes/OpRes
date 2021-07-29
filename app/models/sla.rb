42d301142c79c5980813e2e78c58d5181afea6cd
Azan Abrar <azan.abrar@gmail.com>
2021-07-29 12:26:38 +0500

    minor tweaks on sla model and conformance service

--- app/models/sla.rb
+++ app/models/sla.rb
@@ -5,6 +5,11 @@ class Sla < ApplicationRecord
   # Enums #
   enum support_hours: %i[24/7 24/5 18/7 18/5 other]
 
+  # Constants #
+  SLA_ATTR = %w[service_level_agreement service_level_objective recovery_point_objective recovery_point_objective
+                severity1 severity2 severity3 severity4 severity1_restoration severity2_restoration severity3_restoration
+                severity4_restoration recovery_time_objective
+               ]
   # Validations #
   # validates :service_level_agreement, :service_level_objective, :support_hours, :recovery_point_objective,
   #           :severity1_response_time, :severity2_response_time, :severity3_response_time, :severity4_response_time,
--- app/services/graphs/supplier_conformance_impact_tolerance_service.rb
+++ app/services/graphs/supplier_conformance_impact_tolerance_service.rb
@@ -1,9 +1,6 @@
 class Graphs::SupplierConformanceImpactToleranceService < Graphs::BaseService
   COLORS   = %w[#6BEAB3 #367C5C #CDFAF1 #05b368]
-  SLA_ATTR = %w[service_level_agreement service_level_objective recovery_point_objective recovery_point_objective
-                severity1 severity2 severity3 severity4 severity1_restoration severity2_restoration severity3_restoration 
-                severity4_restoration recovery_time_objective
-               ]
+
   attr_reader :supplier
 
   def call
@@ -25,7 +22,7 @@ class Graphs::SupplierConformanceImpactToleranceService < Graphs::BaseService
     total_sum = 0
     bsls.each do |bsl|
       bsl.sla.attributes.each do |sla_attributes|
-        total_sum = total_sum + sla_attributes.second if SLA_ATTR.include?(sla_attributes.first) && sla_attributes.second.present?       
+        total_sum = total_sum + sla_attributes.second if Sla::SLA_ATTR.include?(sla_attributes.first) && sla_attributes.second.present?       
       end
     end
 
