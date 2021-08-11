5ac40a4714b451ddec2b05b7c36809445acffc98
Ali-eritheialabs <88369210+Ali-eritheialabs@users.noreply.github.com>
2021-08-11 19:17:41 +0500

    Production PR 11-Aug-2021 (#32)
    * [WIP] service object created
    
    * Fixed issue of cloud hosting provider services
    
    * Added third party supplier section, hide service level indicator and minor tweaks for consumption models
    
    * overview charts
    
    * UI updates
    
    * highchart ui updates
    
    * business service line table dynamic data
    
    * dynamic compliance sections with match, meet and exceed logic
    
    * UI fixes
    
    * update font family and ui udpates
    
    * Dynamic all section of supplier resilience page (#30)
    
    Co-authored-by: Azan Abrar <azan.abrar@gmail.com>
    
    Co-authored-by: Ali Ahmad <ali.ahmad@eritheialabs.com>
    Co-authored-by: Azan Abrar <azan.abrar@gmail.com>

--- app/models/sla.rb
+++ app/models/sla.rb
@@ -6,10 +6,10 @@ class Sla < ApplicationRecord
   enum support_hours: %i[24/7 24/5 18/7 18/5 other]
 
   # Constants #
-  SLA_ATTR = %w[service_level_agreement service_level_objective recovery_point_objective recovery_point_objective
-                severity1 severity2 severity3 severity4 severity1_restoration severity2_restoration severity3_restoration
-                severity4_restoration recovery_time_objective
-               ]
+  SLA_ATTR = %w[service_level_agreement service_level_objective recovery_time_objective recovery_point_objective
+                transactions_per_second response_time severity1 severity2 severity3 severity4
+                severity1_restoration severity2_restoration severity3_restoration severity4_restoration]
+
   # Validations #
   # validates :service_level_agreement, :service_level_objective, :support_hours, :recovery_point_objective,
   #           :severity1_response_time, :severity2_response_time, :severity3_response_time, :severity4_response_time,
