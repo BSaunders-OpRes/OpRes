864e55c006bd1809c2cf577049fd9b931f604ecb
Ali Ahmad <ali.ahmad@eritheialabs.com>
2021-08-19 11:43:08 +0500

    host not required in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    host:                 ENV['HOST']
     port:                 587,
     user_name:            ENV['SMTP_EMAIL'],
     password:             ENV['SMTP_PASSWORD'],
