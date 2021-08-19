410c0d7e1c135e8f11a23cc953fecd1921282198
Ali Ahmad <ali.ahmad@eritheialabs.com>
2021-08-19 11:44:25 +0500

    host removed from prodcution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    host:                 ENV['HOST']
     user_name:            ENV['SMTP_EMAIL'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
