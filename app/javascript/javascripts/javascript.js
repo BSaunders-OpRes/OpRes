5988ab582ae0efcfaaf49a5092aea48ae01e7a55
Azan Abrar <azan.abrar@gmail.com>
2021-08-24 18:42:19 +0500

    Finalized Chp section on bsl show page

--- app/javascript/javascripts/javascript.js
+++ app/javascript/javascripts/javascript.js
@@ -7,6 +7,7 @@ import IntroJS    from 'intro.js';
 
 import './journey/journey';
 import './bsl/bsl';
+import './bsl/cloud_hosting_provider'
 import './unit_hierarchy/unit_hierarchy';
 import './supplier/supplier';
 import './administration_portal/administration_portal';
