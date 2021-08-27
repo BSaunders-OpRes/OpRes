254496a03a8c5d5e243a7f339177bb7c84f16054
Ali-eritheialabs <88369210+Ali-eritheialabs@users.noreply.github.com>
2021-08-27 17:37:40 +0500

    Production PR 2nd - 27-Aug-2021 (#72)
    * accordion UI updates with plus minus sign
    
    * Dynamic high risk business service line section on dashboard screen and hid all the sections which are not the part of MVP
    
    * fix bug supplier resilience graph
    
    Co-authored-by: Azan Abrar <azan.abrar@gmail.com>

--- app/services/graphs/system_supplier_resilience_indicator_service.rb
+++ app/services/graphs/system_supplier_resilience_indicator_service.rb
@@ -16,79 +16,78 @@ class Graphs::SystemSupplierResilienceIndicatorService < Graphs::BaseService
   def overall
     datum = {}
     nodes     = organisational_unit.inclusive_children.map(&:id)
-    ptype_suppliers = Supplier.joins(:supplier_steps)
-                              .where(unit_id: nodes)
-                              .select("suppliers.*, supplier_steps.party_type as supplier_party_type")
-                              .group("suppliers.id, suppliers.*, supplier_party_type")
-                              .group_by(&:supplier_party_type)
+    bsls      = BusinessServiceLine.where(unit_id: nodes)
+    @match_firm_hosted_tolerance  =  0
+    @match_3rd_party_tolerance    =  0
+    @match_4th_party_tolerance    =  0
+    @meet_firm_hosted_tolerance   =  0
+    @meet_3rd_party_tolerance     =  0
+    @meet_4th_party_tolerance     =  0
+    @exceed_firm_hosted_tolerance =  0
+    @exceed_3rd_party_tolerance   =  0
+    @exceed_4th_party_tolerance   =  0
 
-    datum[:total]         = ptype_suppliers.values.flatten.size
-    datum[:total_match]   = 0
-    datum[:total_meet]    = 0
-    datum[:total_exceed]  = 0
+    SupplierStep.party_types.keys.each do |step|
+      datum[step.underscore] = {}
+      datum[step.underscore][:total] = 0
+    end
 
-
-    ptype_suppliers.each do |key, suppliers|
-      datum["#{SupplierStep.party_types.key(key).underscore}"] = {}
-      total = 0
-      total_sum = 0
-      @match_tolerance  =  0
-      @meet_tolerance   =  0
-      @exceed_tolerance =  0
-
-      suppliers.each do |supplier|
-        bsls = BusinessServiceLine.joins(steps: [supplier_steps: [:supplier]])
-                                  .where(suppliers: {id: supplier.id})
-                                  .includes(:sla)
-
-        total = total + bsls.count * 120
-
-        bsls.each do |bsl|
+    bsls&.each do |bsl|
+      party_type_suppliers = bsl.supplier_steps.group_by(&:party_type)
+      party_type_suppliers.each do |party_type, supplier_steps|
+        supplier_steps.each do |supplier_step|
+          datum[party_type.underscore][:total] += 1
           bsl.risk_appetites.each do |risk_appetite|
             bsl_sla_val       = bsl.sla[risk_appetite.kind]
-            supplier_sla_val  = supplier.sla[risk_appetite.kind]
+            supplier_sla_val  = supplier_step.supplier.sla[risk_appetite.kind]
             risk_appetite_val = risk_appetite&.amount
             if bsl_sla_val && supplier_sla_val && risk_appetite_val
               if risk_appetite.percentage_amount?
-                result = find_score_and_status_for_percentage(bsl.sla[risk_appetite.kind],  supplier.sla[risk_appetite.kind], risk_appetite.amount)
-                self.instance_variable_set("@#{result[1]}_tolerance".to_sym, eval("@#{result[1]}_tolerance")+1)
+                result = find_score_and_status_for_percentage(bsl.sla[risk_appetite.kind],  supplier_step.supplier.sla[risk_appetite.kind], risk_appetite.amount)
+                self.instance_variable_set("@#{result[1]}_#{party_type.underscore}_tolerance".to_sym, eval("@#{result[1]}_#{party_type.underscore}_tolerance")+1)
               else
-                result = find_score_and_status_for_time(bsl.sla[risk_appetite.kind],  supplier.sla[risk_appetite.kind], risk_appetite.amount)
-                self.instance_variable_set("@#{result[1]}_tolerance".to_sym, eval("@#{result[1]}_tolerance")+1)
+                result = find_score_and_status_for_time(bsl.sla[risk_appetite.kind],  supplier_step.supplier.sla[risk_appetite.kind], risk_appetite.amount)
+                self.instance_variable_set("@#{result[1]}_#{party_type.underscore}_tolerance".to_sym, eval("@#{result[1]}_#{party_type.underscore}_tolerance")+1)
               end
             end
           end
         end
       end
-
-      datum["#{SupplierStep.party_types.key(key).underscore}"][:total]     = suppliers.count
-      datum[:total_exceed]  += @exceed_tolerance
-      datum[:total_match]   += @match_tolerance
-      datum[:total_meet]    += @meet_tolerance
-      #datum["#{SupplierStep.party_types.key(key).underscore}"][:total_bsl] = bsls.size
-      datum["#{SupplierStep.party_types.key(key).underscore}"][:graph]     = []
-
-      datum["#{SupplierStep.party_types.key(key).underscore}"][:graph] << {
+    end
+    total_sum = 0
+    total_match = 0
+    total_meet = 0
+    total_exceed = 0
+    SupplierStep.party_types.keys.each do |step|
+      total         = eval("@match_#{step.underscore}_tolerance") + eval("@meet_#{step.underscore}_tolerance") + eval("@exceed_#{step.underscore}_tolerance")
+      total_sum     += total
+      total_match   += eval("@match_#{step.underscore}_tolerance")
+      total_meet    += eval("@meet_#{step.underscore}_tolerance")
+      total_exceed  += eval("@exceed_#{step.underscore}_tolerance")
+      datum[step.underscore][:graph] = []
+      datum[step.underscore][:graph] << {
         name:  'Match Tolerance',
-        y:     datum["#{SupplierStep.party_types.key(key).underscore}"][:total].zero? ? 0 : ((@match_tolerance / datum["#{SupplierStep.party_types.key(key).underscore}"][:total].to_f) * 100).round(2),
+        y:     datum[step.underscore][:total].zero? ? 0 : ((eval("@match_#{step.underscore}_tolerance") / total.to_f) * 100).round(2),
         color: COLORS[0],
-        count: @match_tolerance
+        count: eval("@match_#{step.underscore}_tolerance")
       }
-
-      datum["#{SupplierStep.party_types.key(key).underscore}"][:graph] << {
+      datum[step.underscore][:graph] << {
         name:  'Meet Tolerance',
-        y:     datum["#{SupplierStep.party_types.key(key).underscore}"][:total].zero? ? 0 : ((@meet_tolerance / datum["#{SupplierStep.party_types.key(key).underscore}"][:total].to_f) * 100).round(2),
+        y:     datum[step.underscore][:total].zero? ? 0 : ((eval("@meet_#{step.underscore}_tolerance") / total.to_f) * 100).round(2),
         color: COLORS[1],
-        count: @meet_tolerance
+        count: eval("@meet_#{step.underscore}_tolerance")
       }
-
-      datum["#{SupplierStep.party_types.key(key).underscore}"][:graph] << {
+      datum[step.underscore][:graph] << {
         name:  'Exceed Tolerance',
-        y:     datum["#{SupplierStep.party_types.key(key).underscore}"][:total].zero? ? 0 : ((@exceed_tolerance / datum["#{SupplierStep.party_types.key(key).underscore}"][:total].to_f) * 100).round(2),
-        color: COLORS[2],
-        count: @exceed_tolerance
+        y:     datum[step.underscore][:total].zero? ? 0 : ((eval("@exceed_#{step.underscore}_tolerance") / total.to_f) * 100).round(2),
+        color: COLORS[1],
+        count: eval("@exceed_#{step.underscore}_tolerance")
       }
     end
+    datum[:total]         = total_sum
+    datum[:total_match]   = total_match
+    datum[:total_meet]    = total_meet
+    datum[:total_exceed]  = total_exceed
     datum
   end
 end
