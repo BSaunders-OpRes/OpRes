eeb9c89d610ec13b092f2482bd8108ba42b7c1d4
Ali Ahmad <ali.ahmad@eritheialabs.com>
2021-09-13 18:21:10 +0500

    filters are ajax based

--- app/javascript/javascripts/critical_important_system/cis.js
+++ app/javascript/javascripts/critical_important_system/cis.js
@@ -15,20 +15,39 @@ document.addEventListener('turbolinks:load', function() {
 
   $('#clear-field').on('click', function(){
     $('#search-filter').val('');
-
   });
 
-  $(".filter-value").on('click', function(){
-    $.ajax({
-      url:      '/organisation/dashboard_break_downs/critical_important_system',
-      dataType: 'script',
-      type:     'GET',
-      async:     false,
-      data:     {
-        critical: $(this).data('args')
-      }
+  $(".segmentation-options input[type=checkbox]").on('click', function(){
+    if (this.checked){
+      segmentation_suppliers = getFilterValues($(".segmentation-options .segmentation_suppliers input:checked"))
+      segmentation_regions = getFilterValues($(".segmentation-options .segmentation_regions input:checked"))
+      segmentation_countries = getFilterValues($(".segmentation-options .segmentation_countries input:checked"))
+      segmentation_firms = getFilterValues($(".segmentation-options .segmentation_firms input:checked"))
+      segmentation_products = getFilterValues($(".segmentation-options .segmentation_products input:checked"))
+      
+      $.ajax({
+        url:      '/organisation/dashboard_break_downs/critical_important_system',
+        dataType: 'script',
+        type:     'GET',
+        async:     false,
+        data: {
+          supplier_type: segmentation_suppliers,
+          regions: segmentation_regions,
+          countries: segmentation_countries,
+          firms: segmentation_firms,
+          products: segmentation_products
+        }
+      });
+    }
+  });
+
+  function getFilterValues(filterObject) {
+    dataFilters = []
+    filterObject.map(function( i, e ) {
+      dataFilters.push(e.getAttribute('data-args'));
     });
-  });
+    return dataFilters;
+  }
 });
 
 
