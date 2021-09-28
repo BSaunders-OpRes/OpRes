bfb32857284d6ba7f56248162312b63dcbee190f
Azan Abrar <azan.abrar@gmail.com>
2021-09-28 17:17:42 +0500

    segmentation options on csp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -170,12 +170,12 @@ module ApplicationHelper
   def overview_heading(filters)
     if filters.present? && filters.dig("party_type_ids").present?
       if filters.dig("party_type_ids").include?("all")
-        "Total Number Of Suppliers"
+        "Total Number Of Impact Tolerance Suppliers"
       else
-        "Total Number Of #{filters.dig("party_type_ids").join(' & ')} Suppliers"
+        "Total Number Of #{filters.dig("party_type_ids").join(' & ')} Impact Tolerance Suppliers"
       end
     else
-      "Total Number Of Suppliers"
+      "Total Number Of Impact Tolerance Suppliers"
     end
   end
 
