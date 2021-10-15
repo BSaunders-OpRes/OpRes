494764823c2e50d388f3b40253febec3085e37aa
Azan Abrar <azan.abrar@gmail.com>
2021-10-15 14:16:35 +0500

    remove unused method

--- app/services/journey/search_service.rb
+++ app/services/journey/search_service.rb
@@ -11,7 +11,6 @@ class Journey::SearchService < ApplicationService
     bsls_data       = get_bsls_data
     suppliers_data  = get_suppliers_data
     resilience_data = get_resilience_data
-    searched_data   = get_searched_data
     data << bsls_data << suppliers_data << resilience_data
 
     return data.flatten
@@ -31,12 +30,7 @@ class Journey::SearchService < ApplicationService
 
   def get_resilience_data
     resilience_ticket = ResilienceTicket.where(unit_id: @nodes)
-                                        .where("CAST(rgid AS CHAR) ILIKE (?)", "%#{@filter}%")
+                                        .where("rgid ILIKE (?)", "%#{@filter}%")
     return resilience_ticket.map {|rt| {id: rt.id, name: rt.rgid, url: Rails.application.routes.url_helpers.organisation_resilience_ticket_path(rt.id)}}
-  end
-
-  def get_searched_data
-    search_histories = @current_user.search_histories
-    return search_histories.map {|sh| {id: sh.id, name: sh.title, url: sh.url}}
   end
 end
